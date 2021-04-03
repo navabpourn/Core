5a0915fb5e8cc07bb3877673f7f835c887e6e963
Nafiseh <nafiseh.navabpour@uni-jena.de>
2021-04-03 20:44:17 +0200

    Add DQM Module

---
+++ Console/BExIS.Web.Shell/Areas/DQM/BExIS.Modules.DQM.UI/Helper/DQMSeedDataGenerator.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace BExIS.Modules.DQM.UI.Helpers
+{
+    public class DQMSeedDataGenerator : IDisposable
+    {
+        public void GenerateSeedData()
+        {
+
+        }
+
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
